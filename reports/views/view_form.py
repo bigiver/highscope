52d260fcf82343225a23a46b353033b768ce4e67
dajiang <huaigang.zhao@outlook.com>
2016-10-13 23:24:37 +0800

    绑定下拉列表

--- reports/views/view_form.py
+++ reports/views/view_form.py
@@ -8,8 +8,8 @@ from reports.models import Class,Students,Teacher,Domain,Type,Level
 
 #填写页
 def formView(request):
-	domain = list(Domain.objects.values_list())
+	domain = list(Domain.objects.values())
 	types = list(Type.objects.values())
 	level = list(Level.objects.values())
 
-	return render(request,'form_basic.html',{'domain':json.dumps(domain),'types':json.dumps(types),'level':json.dumps(level)})
+	return render(request,'form_basic.html',{'domain':json.dumps(domain,ensure_ascii=False),'types':json.dumps(types,ensure_ascii=False),'level':json.dumps(level,ensure_ascii=False)})
